ae37a462b4f3d84c971904b667df14948d7d248e
Guilherme Magalhaes <gmagalhaes@MacBook-Pro-de-Guilherme.local>
2015-11-11 18:27:41 -0200

    init(intial) : Initial Commit
    Commit initial for the new version of carminp
    
    Closes #NoIssue

---
+++ src/test/java/me/achv/carminp/api/CarminpNosqlApiApplicationTests.java
@@ -0,0 +1,16 @@
+package me.achv.carminp.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CarminpNosqlApiApplication.class)
+public class CarminpNosqlApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
